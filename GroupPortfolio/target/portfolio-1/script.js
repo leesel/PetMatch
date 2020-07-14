811228755c1d130b5b0a3a0e953410acc5a97884
Fareeda <kasimfareeda@gmail.com>
2020-07-14 19:47:36 -0400

    Created html and css files for sign in and home page

--- GroupPortfolio/target/portfolio-1/script.js
+++ GroupPortfolio/target/portfolio-1/script.js
@@ -1,81 +1,26 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-async function getComment() {
-    fetch('/data').then(response => response.json()).then((myObject) => {
-        const commentsContainer = document.getElementById('recent-comments');
-        myObject.forEach((comment) => {
-            commentsContainer.appendChild(createDivElement(comment.name, comment.comment));
-        });
-});
+function getContent() {
+    console.log('Retrieving content...')
+    fetch('/data').then(response => response.text()).then(responseText => JSON.parse(responseText)).then(dat =>{
+        var badge = document.getElementById("badge");
+        //badge.innerHTML = '<ol id="posts-list" class="hfeed">';
+        var badgeStr = '<ol id="posts-list" class="hfeed">';
+        //document.getElementById('comment-container').innerText = response.text();
+        for (let comment_obj of dat){
+            var name = comment_obj.name;
+            var comment = comment_obj.comment;
+            var score = comment_obj.score;
+            var timestamp = comment_obj.timestamp;
+            var date = new Date(timestamp * 1000);
+            // Hours part from the timestamp
+            var hours = date.getHours();
+            // Minutes part from the timestamp
+            var minutes = "0" + date.getMinutes();
+            // Seconds part from the timestamp
+            var seconds = "0" + date.getSeconds();
+            // Will display time in 10:30:23 format
+            var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            badgeStr += '<li><article id="comment_" class="hentry">' + '<footer class="post-info">'+ '<div class="published">'+formattedTime+'</div>'+'<address class="author">'+'By '+ name+'</address>'+'</footer>'+'<div class="entry-content">'+'<p>'+comment+'</p>'+'</div>'+'</article></li>';
+        }
+        badge.innerHTML = badgeStr + '</ol>';
+    });
 }
-
-/** Creates a <div> element containing text. */
-function createDivElement(text1, text2) {
-  const divElement = document.createElement('div');
-  divElement.setAttribute("class", "each-comment");
-  var words = " left a comment: ";
-  divElement.innerText = text1 + words + text2;
-  return divElement;
-}
-
-/**function fetchBlobstoreUrlAndShowForm() {
-  fetch('/blog')
-      .then((response) => {
-        return response.text();
-      })
-      .then((imageUploadUrl) => {
-        const messageForm = document.getElementById('image-form');
-        messageForm.action = imageUploadUrl;
-      }).then(response => response.json()).then((myObject) => {
-        const imagesContainer = document.getElementById('all-images');
-        myObject.forEach((image) => {
-            console.log(image);
-            imagesContainer.appendChild(createDivImageElement(image));
-        });
-      });
-}*/
-
-function fetchBlobstoreUrlAndShowForm() {
-    fetch('/blog')
-    .then(response => response.json())
-    .then((images) => {
-        //const messageForm = document.getElementById('image-form');
-        //messageForm.action = images;
-        const imagesContainer = document.getElementById('all-images');
-        images.forEach((image) => {
-            console.log(image.imageURL);
-            imagesContainer.appendChild(createDivImageElement(image.imageURL));
-        });
-      });
-}
-
-/**i thought i was allowed to have two promises/fetch functions? */
-
-/**async function displayImages() {
-    fetch('/data').then(response => response.json()).then((myObject) => {
-        const commentsContainer = document.getElementById('recent-comments');
-        myObject.forEach((comment) => {
-            commentsContainer.appendChild(createDivElement(comment.name, comment.comment));
-        });
-});
-}*/
-
-function createDivImageElement(image) {
-  const divElement = document.createElement('div');
-  divElement.setAttribute("class", "each-image");
-  divElement.innerHTML = "<img src=\"" + image + "\" />";
-  return divElement;
-}
-
